a5126aca22f599cc7feb990b083fa7816ce209d1
Parikshit <singhp154154@gmail.com>
2017-12-18 23:42:44 +0530

    Add till Set

--- learn.js
+++ learn.js
@@ -358,4 +358,366 @@ for (var i = 1; i <= 10; i++) {
     }
     console.log(i);
 }
+/*============================================FUNCTIONS============================================*/
+//creating a function
+function introduction() {
+    console.log("hello world form js-cheetsheet functions");
+}
+//calling
+introduction();
+//parameters
+function para(text){
+    console.log("displaying " + text);
+}
+para(text);
+//return 
+//Functions can return data to us.
+function getAreaOfCircle(radius) {
+    return Math.PI * radius * radius;
+}
 
+var radius = 3;
+var area = getAreaOfCircle(radius);
+console.log("A circle with a radius of " + radius + " has an area of " + area + ".");
+
+/*==========================================ARRAYS=================================================*/
+/*
+    Data type that is built using primitive data types and that is an array. Arrays are a list of a
+    data type that can then be stored in a single variable.
+*/
+// creation
+var drinks = ["water", "juice", "milk", "soda"];
+//retreving 
+console.log(drinks[0]);
+for (var i = 0; i < drinks.length; i++) {
+    console.log(drinks[i]);
+}
+//reassignment
+drinks[2] = "coffee";
+console.log(drinks);
+//ARRAY METHODS 
+//Sort
+/*
+    You can sort you array by calling the sort method on it.
+*/
+
+var sortedDrinks = drinks.sort();
+console.log(sortedDrinks);
+//Push
+/*
+    With push, you can easily add new items to the end of your array. Just call push on your array
+    with the new item you want to add.
+*/
+drinks.push("tea");
+console.log(drinks);
+//Pop
+/*
+    Pop is the opposite of push. Call pop on your array to remove the last item in your array.
+*/
+drinks.pop(); // remove the last item in this array
+
+console.log(drinks);
+//Shift
+/*
+    The shift method removes the first element of an array and then returns that element to you.
+*/
+var chocolates = ["Snickers", "Kit-Kat", "Twix"];
+
+var snack = chocolates.shift();
+
+console.log(snack);
+console.log(chocolates);
+//Unshift
+/*
+    The unshift method adds elements to the beginning of the array and returns the new array's
+    length to you.
+*/
+var chocolates = ["Snickers", "Kit-Kat", "Twix"];
+
+var newLength = chocolates.unshift("Milky Way", "3 Musketeers");
+
+console.log(newLength);
+console.log(chocolates);
+//Concat
+/*
+    You can create a new array by concatenating two existing arrays. All you need to do is call 
+    concat on one array, with the second array being the parameter.
+*/
+var healthyDrinks = ["water", "milk"];
+var unhealthyDrinks = ["juice", "soda"];
+
+var allDrinks = healthyDrinks.concat(unhealthyDrinks);
+console.log(allDrinks);
+//Slice
+/*
+    Using slice, you can return a brand new array containing elements from a slice of the original
+    array. It's pretty simple to use, simply provide the method with the starting and ending indexes
+    and it will do the rest.
+*/
+var colors = ["purple", "red", "white", "blue", "gold"];
+
+var slice = colors.slice(1, 4);
+console.log(slice);
+//If you only provide one parameter, it will use that number as the starting point, and copy the rest of the array.
+slice = colors.slice(3);
+console.log(slice);
+//Finally, if you provide a negative index, it will just start from the end and go backwards.
+slice = colors.slice(-3);
+console.log(slice);
+//splice 
+//If you don't want to return a new array, you can use splice.
+var foods = ["burger", "pizza", "donut", "bread", "pasta"];
+
+foods.splice(2);
+console.log(foods);
+/*
+    If you provide a second parameter, you can specify the number of elements to remove from the
+    array starting from that index.
+*/
+foods = ["burger", "pizza", "donut", "bread", "pasta"];
+
+foods.splice(2, 1);
+console.log(foods);
+//MAP
+/*
+    When you want to iterate over an array, you could use a loop. However, you could also use map.
+*/
+ var names = ["Ram", "Shyam", "Pari"];
+
+    var intros = names.map(function(name) {
+    	return "My name is " + name;
+    });
+
+    console.log(intros);
+//filter
+/*
+    The filter method is useful for, well, filtering out elements you don't want from an array to
+    form a new one.
+*/
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+var odd = numbers.filter(function (number) {
+    return number % 2 == 1;
+});
+
+console.log(odd);
+//Reduce
+/*
+    reduce goes over every element and collects some information about each element, and then finally 
+    returns the result.
+*/
+/*
+    A simple way to demonstrate reduce is to use it to sum up all the numbers in an array.
+    or according to medium :
+    "Reduce Method In JavaScript​ gives you a mini CodePen where you can write whatever logic you want. 
+    It will repeat the logic for each amount in the array and then return a single value."
+*/
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+var total = numbers.reduce(function (total, number) {
+    return total + number;
+});
+
+console.log(total);
+/*
+    detailed explation for those who are new to functional programming
+    note: reduce method loop thorugh each member of array just like for loop
+    so, when loop starts the value of total is at the far left(i.e 1) and number is the current
+    value (i.e 2) and in this example we want to add total with the number every time we loop 
+    this is whay we use "toatal + number" and calculation is repeated for each number of array but
+    each time current value changes to next number
+    When there are no more numbers left in the array the method returns the total value.
+*/ 
+//more examples of .reduce
+//average using reduce
+const average = numbers.reduce((total, amount, index, array)=>{
+    total += amount;
+    if( index === array.length-1){
+        return total/array.length;
+    }else{
+        return "Unexpacted error";
+    }
+});
+//index will give index of a given array and array will give number array itself
+//The thing is, you don’t always have to return a single value. You can reduce an array into a new array.
+
+/*
+    For instance, lets reduce an array of amounts into another array where every amount is doubled.
+    **To do this we need to set the initial value for our accumulator to an empty array.
+    
+    The initial value is the value of the total parameter when the reduction starts. You set the
+    initial value by adding a comma followed by your initial value inside the parenthesis but after
+    the curly braces
+    const average = numbers.reduce((total, number, index, array)=>{
+        total + amount;
+        return total/array.length;
+    }, 0);
+*/
+
+/*
+    By setting the initial value to an empty array we can then push each amount into the total.
+    If we want to reduce an array of values into another array where every value is doubled, 
+    we need to push the amount * 2. Then we return the total when there are no more amounts to push.
+*/
+const double = numbers.reduce((total, amount, index, array)=>{
+    return total;
+    total.push(amount*2);
+},[]);
+console.log(double);
+/*
+    The benefit of using reduce comes into play when you want to map and filter together and you have
+    a lot of data to go over.
+    for more info hit to https://medium.freecodecamp.org/reduce-f47a7da511a9
+    */
+/*=========================================OBJECTS=================================================*/
+/*
+An object in JavaScript is a collection of attributes that collectively describe a "thing".
+That "thing" could be anything you want: a pencil, a person, a hat, or a car.
+*/
+// creation 
+var dog = {
+    name: "Cooper",
+    age: 3,
+    bark: function () {
+        console.log("BORK BORK");
+    }
+};
+/*
+    This creates a new object with the properties name, age, and bark, and initializes the variable
+    dog to that.
+*/
+//retrival
+console.log(dog.name);
+console.log(dog.age);
+dog.bark();
+//reassignment
+dog.age = 4;
+dog.weight = 20;
+//METHODS
+//hasOwnProperty
+/*
+    As the name implies, the hasOwnProperty method lets you check if an object has a property or not.
+*/
+console.log(dog.hasOwnProperty("name"));
+console.log(dog.hasOwnProperty("notHere"));
+//key
+//instead of checking some parameter you can check all the properties 
+console.log(Object.keys(dog));
+//values
+/*
+    insted of all the keys you want all the values you can use values method
+*/
+console.log(Object.values(dog));
+//entries
+/*
+    if you want both keys and values use entries method
+*/
+console.log(Object.entries(dog));
+/*============================================CLASSES==============================================*/
+/*
+Classes in JavaScript provide a blueprint for the creation of objects. This means that once you
+define a class (including its member variables and methods), you can endlessly create new objects
+from it. This is useful because it allows us to reduce the amount of code we need to write when we
+want multiple objects that are similar to one another.
+*/
+/*
+                        class pet {
+                        }
+                        var cow = new pet();
+                        consoel.log(cow + "this is new object from class");
+*/
+// constructing class
+class Pet { // convention P is uppercase
+    constructor(name, weight, price){
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+    }
+
+    getInfo(){
+        return "Hii, my name is " + this.name + ", I weigh " + this.weight + "kgs and I worth " + this.price + " Rs";
+    }
+}
+
+var Cow = new Pet("radha", 750, 15000);
+console.log(Cow);
+console.log(Cow.getInfo());
+//Class inheretanc 
+/*
+    While our current solution works for now, it can certainly be improved upon. Our Pet class doesn't
+    offer any distinction between cow and dog, which each would have additional information that
+    doesn't apply to the other. Luckily for us, JavaScript offers class inheritance, which means we
+    can create new classes that inherit everything from another, but adds unique information for 
+    itself. This means that since a dog is still a pet, we can just inherit from the Pet class and
+    add any dog-specific stuff in the Dog class.
+*/
+class Dog extends Pet {
+    bark() {
+        console.log("BARK BARK BARK");
+    }
+}
+//created a new clas which inherets all the properties of Pet
+var dog = new Dog("Chubby", 4, 10000);
+console.log(dog.getInfo());
+console.log(dog.bark());
+//function overriding 
+class Cat extends Pet {
+    constructor(name, weight, price, brand){
+        super(name, weight, price);
+        brand = this.brand;
+    }
+    getFavourateBrand() {
+        return this.brand;
+    }
+    getInfo(){
+        return "Hi, I'm a cat named " + this.name + " and I like " + this.brand + ". I weigh " + this.weight + " pounds and cost " + this.price + " Rs";
+    }
+}
+
+var honey  = new Cat ("honey", 7, 1500, "Catchip");
+console.log(honey.getInfo());
+console.log(honey.getFavourateBrand());
+/*
+    This is simply manually calling the constructor of the class that Cat is inheriting from, in this
+    case, the Pet class. The only reason this is needed is because we added a new parameter to the 
+    constructor of Cat, and that is the brand string.
+    and we override the getInfo function
+*/
+/*=========================================SETS====================================================*/
+/*
+    Sets are an object type that let you create a collection of values that must be unique. 
+    This is useful in cases when you are faced with potential duplicates, but only want to store a
+    single instance of that value. Any duplicates are ignored.
+*/
+//creating a set
+var set = new Set();//S capital
+//adding is similar as adding element to an array 
+set.add("Twitter");
+set.add("Facebook");
+set.add("LinkedIn");
+set.add("Facebook");
+console.log(set);
+// {"Facebook", "Twitter", "LinkedIn"} facebook was added twise but Ii dident displaay because it was 
+//already there no duplicates thats what sets is
+//size
+/*
+    Sets give you a very straight-forward way to get back the size of your set. Simply use the size
+    property.
+*/
+console.log(set.size);
+//has
+/*
+    When you want to check if a set contains an element, call the has method on it.
+    return bool type
+*/
+console.log(set.has("Facebook"));
+console.log(set.has("Instagram"));
+// Delete
+set.delete("Facebook");
+//Clear
+/*
+    To wipe a set clean of all of its elements, call the clear method on it. It resets a set as if
+    you had just created a new one.
+*/
+set.clear(); //RIP elements
+/*==========================================MAPS===================================================*/
